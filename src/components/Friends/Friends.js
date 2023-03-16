0a4ff04321c9617a52e7926c21d9227fc2a3eef6
fatihguzel <20060393@stu.omu.edu.tr>
2023-03-16 23:02:13 +0300

    #1 Anasayfa Oluşturuldu, Navigation Eklendi

---
+++ src/components/Friends/Friends.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const FriendsPage = () => {
+  return (
+    <View>
+      <Text>FriendsPage</Text>
+    </View>
+  );
+};
+
+export default FriendsPage;
